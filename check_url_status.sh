fef64c2981bf76417f1ebdff6ce3fb630debaa6f
Naveen Kumar <naveen.m.787@gmail.com>
2023-10-22 15:27:14 +0530

    Update check_url_status.sh

--- check_url_status.sh
+++ check_url_status.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+interrupted() {
+    echo "Script execution interrupted by the user."
+    exit 1
+}
+
+trap interrupted SIGINT
+
 check_url() {
     url="$1"
+    codes="$2"
+
     response_code=$(python3 - <<END
 import requests
 url = "$url"
@@ -9,27 +18,39 @@ response = requests.get(url)
 print(response.status_code)
 END
 )
-    if [ "$response_code" == "200" ]; then
-        echo "URL $url returns a 200 (OK) status code"
-    else
+
+    if [ -z "$codes" ] || echo "$codes" | grep -q "$response_code"; then
         echo "URL $url returns a $response_code status code"
     fi
 }
 
 check_urls_in_file() {
     input_file="$1"
+    codes="$2"
     while IFS= read -r url; do
-        check_url "$url"
+        check_url "$url" "$codes"
     done < "$input_file"
 }
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <URL or input_file>"
+    echo "Usage: $0 <URL or input_file> [status_codes] [output_file]"
     exit 1
 fi
 
-if [ -f "$1" ]; then
-    check_urls_in_file "$1"
+input="$1"
+codes="$2"
+output_file="$3"
+
+if [ -f "$input" ]; then
+    if [ -n "$output_file" ]; then
+        check_urls_in_file "$input" "$codes" > "$output_file"
+    else
+        check_urls_in_file "$input" "$codes"
+    fi
 else
-    check_url "$1"
+    if [ -n "$output_file" ]; then
+        check_url "$input" "$codes" > "$output_file"
+    else
+        check_url "$input" "$codes"
+    fi
 fi
